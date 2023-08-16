56718d461be4dcbeb3ad7e325b28db7d4a8a7aaf
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-16 13:07:42 -0300

    Faculdade

--- conceitosBasicos/Program.cs
+++ conceitosBasicos/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
+
+int num1 = 20;
+string num2 = "22";
+string soma = num2 + num2;
+Console.WriteLine($"A soma entre {num1} e {num2} é {soma}");
+
+float pi = 3.141697f;
+decimal salario = 4343535.34334m;
+
+Console.WriteLine("PI: {0}", pi.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine($"salario {salario}");
