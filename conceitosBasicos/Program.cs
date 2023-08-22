cc57698c2ad3cca9dc03af8e519cac3aa8d19d0c
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-22 20:10:50 -0300

    Faculdade

--- conceitosBasicos/Program.cs
+++ conceitosBasicos/Program.cs
@@ -1,15 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Globalization;
+﻿//// See https://aka.ms/new-console-template for more information
+//using System.Globalization;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+//int num1 = 20;
+//string num2 = "22";
+//string soma = num2 + num2;
+//Console.WriteLine($"A soma entre {num1} e {num2} é {soma}");
+
+//float pi = 3.141697f;
+//decimal salario = 4343535.34334m;
+
+//Console.WriteLine("PI: {0}", pi.ToString(CultureInfo.InvariantCulture));
+//Console.WriteLine($"salario {salario}");
+
+using conceitosBasicos;
 
 int num1 = 20;
-string num2 = "22";
-string soma = num2 + num2;
-Console.WriteLine($"A soma entre {num1} e {num2} é {soma}");
+int num2 = 11, result;
 
-float pi = 3.141697f;
-decimal salario = 4343535.34334m;
+Somador soma;
+soma = new Somador();
 
-Console.WriteLine("PI: {0}", pi.ToString(CultureInfo.InvariantCulture));
-Console.WriteLine($"salario {salario}");
+result = soma.Soma(num1, num2);
+Console.WriteLine(result);
