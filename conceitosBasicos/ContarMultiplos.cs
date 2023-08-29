b4d796b6b1e3a9794d8746f33b116016606c7130
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-29 20:38:58 -0300

    Faculdade

--- conceitosBasicos/ContarMultiplos.cs
+++ conceitosBasicos/ContarMultiplos.cs
@@ -20,21 +20,3 @@ namespace conceitosBasicos
     }
 }
 
-
-//public: É um modificador de acesso que indica que o método pode ser acessado por outras classes fora da própria classe onde ele está definido. 
-// sso significa que o método é visível e pode ser chamado de qualquer lugar onde a classe que contém esse método seja acessível.
-
-//bool: É o tipo de retorno do método. Isso significa que quando o método é chamado, ele retornará um valor do tipo bool, que representa um valor 
-//lógico (true ou false).
-
-//Multiplo: É o nome do método. Nesse contexto, o nome sugere que o método será usado para verificar se um número é um múltiplo de outro número.
-
-//(int numero, int divisor): Entre parênteses, estão os parâmetros do método. Nesse caso, o método recebe dois parâmetros: numero e divisor, 
-//ambos do tipo int (inteiro).
-
-//numero: Este é um parâmetro que representa o número que será verificado para determinar se ele é um múltiplo.
-//divisor: Este é um parâmetro que representa o número pelo qual vamos verificar se o numero é um múltiplo.
-
-
-//Esse código é um exemplo de um namespace chamado "Multiplos3" que contém uma classe chamada "MultiplosCalcular". A classe "MultiplosCalcular" 
-//possui um método público chamado "Multiplo" que verifica se um número é um múltiplo de 3
--- conceitosBasicos/Program.cs
+++ conceitosBasicos/Program.cs
@@ -67,23 +67,3 @@ using conceitosBasicos;
     
  
 
-//Instanciação da Classe MultiplosCalcular: Uma instância da classe MultiplosCalcular é criada usando MultiplosCalcular 
-//calcular = new MultiplosCalcular();. Isso permitirá que usemos os métodos definidos na classe MultiplosCalcular.
-
-//Contagem de Múltiplos: Um loop for é usado para iterar de 0 a 100. Para cada valor i no intervalo, verifica-se se é um múltiplo de 3 usando o
-//método calcular.Multiplo(i, 3) da instância da classe MultiplosCalcular.Se for, a variável multiplos é incrementada.
-
-//Exibição do Resultado: Após o loop, a quantidade de múltiplos de 3 encontrados é exibida no console usando Console.WriteLine.
-
-//MultiplosCalcular calcular = new MultiplosCalcular(); = MultiplosCalcular calcular = new MultiplosCalcular();: Aqui, estamos criando uma instância 
-//da classe MultiplosCalcular e a atribuindo a uma variável chamada calcular. Essa instância será usada para chamar o método Multiplo mais tarde.
-
-//int multiplos = 0; = int multiplos = 0;: Esta linha declara uma variável inteira chamada multiplos e a inicializa com o valor zero. Essa variável 
-//será usada para contar quantos números múltiplos de 3 são encontrados no intervalo.
-
-//for (int i = 0; i <= 100; i++) = for (int i = 0; i <= 100; i++): Este é um loop for que itera de 0 a 100, inclusive. Ele declara uma variável i 
-//que começa em 0, e enquanto i for menor ou igual a 100, o loop continuará executando. A cada iteração, i é incrementado em 1.
-
-//if (calcular.Multiplo(i, 3)) = if (calcular.Multiplo(i, 3)): Aqui, estamos verificando se o número i é um múltiplo de 3 usando o método Multiplo da 
-// instância calcular. O método Multiplo retorna true se i for múltiplo de 3 e false caso contrário.
-//multiplos++; = Se o número i for um múltiplo de 3, incrementamos a variável multiplos em 1
