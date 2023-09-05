16dab2b1c9e3920420ef1995a11c2234b6eeb6bf
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-09-05 20:51:57 -0300

    Faculdade

---
+++ controleContas/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    public class Conta
+    {
+        public Conta(long numero) 
+        {
+            this.Numero = numero;
+        }
+        public Conta() { }
+        public long Numero { get; private set; }
+        public decimal Saldo { get; set; }
+    }
+}
---
+++ controleContas/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using controleContas;
+
+Conta conta1 = new Conta(123456,0);
+Conta conta2 = new Conta(654321,0);
+conta2.Saldo = 2341.42m;
+conta1.Saldo = conta2.Saldo * 0.7m;
+
+
+//conta1._numero = 123456;
+
+Console.WriteLine($"Número da conta: {conta1.Numero} com saldo {conta1.Saldo}");
+Console.WriteLine($"Número da conta: {conta2.Numero} com saldo {conta2.Saldo}");
